0f2c0786e09a6c4ecacf1431dbf64549d2e3f199
tk <tk@midibox.org>
2008-01-31 20:43:58 +0000

    first app_lcd modules

---
+++ modules/app_lcd/dummy/app_lcd.mk
@@ -0,0 +1,12 @@
+# $Id: $
+# defines the rule for creating the app_lcd.o object, or assembling the app_lcd.inc file directly
+
+# add this directory to include path
+GPASM_INCLUDE += -I $(MIOS_PATH)/modules/app_lcd/dummy
+
+# make rule for relocatable version
+$(OUTDIR)/app_lcd.o: $(MIOS_PATH)/modules/app_lcd/dummy/app_lcd.asm
+	$(GPASM) $(GPASM_FLAGS) $(GPASM_INCLUDE) $(GPASM_DEFINES) $(APP_LCD_DEFINES) $< -o $@
+
+# directories and files that should be part of the distribution (release) package
+DIST += $(MIOS_PATH)/modules/app_lcd/dummy
