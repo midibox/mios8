38399eebd9407e8ec8c2c39e5411a5bdf9c88a49
tk <tk@midibox.org>
2008-02-01 00:32:17 +0000

    corrected headers

--- apps/processing/analog_toolbox/main.c
+++ apps/processing/analog_toolbox/main.c
@@ -4,26 +4,10 @@
  *
  * ==========================================================================
  *
- * Copyright (C) 2005  Thorsten Klose (tk@midibox.org)
+ *  Copyright (C) 2005  Thorsten Klose (tk@midibox.org)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
  * 
- * ==========================================================================
- * 
- * This file is part of a MIOS application
- *
- * This application is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This application is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with This application; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  * ==========================================================================
  */
 
