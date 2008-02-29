1de071b5ee152842fae0e7c60b1c64a22d2ba366
tk <tk@midibox.org>
2008-02-29 00:12:53 +0000

    new AOUT driver (C version will follow later)

--- modules/aout/aout.mk
+++ modules/aout/aout.mk
@@ -1,11 +1,12 @@
 # $Id$
 # defines the rule for creating the aout.o object
 
+GPASM_INCLUDE += -I $(MIOS_PATH)/modules/aout 
 SDCC_INCLUDE += -I $(MIOS_PATH)/modules/aout 
 
 # make rule
-$(OUTDIR)/aout.o: $(MIOS_PATH)/modules/aout/aout.c
-	$(SDCC) $(SDCC_FLAGS) $(SDCC_INCLUDE) $(SDCC_DEFINES) $(AOUT_DEFINES) $< -o $@
+$(OUTDIR)/aout.o: $(MIOS_PATH)/modules/aout/aout.asm
+	$(GPASM) $(GPASM_FLAGS) $(GPASM_INCLUDE) $(GPASM_DEFINES) $(AOUT_DEFINES) $< -o $@
 
 # directories and files that should be part of the distribution (release) package
 DIST += $(MIOS_PATH)/modules/aout
