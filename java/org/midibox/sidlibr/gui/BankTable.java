45bbd9ad36bb2d514c85b0ae4c10ca0de394f9c2
tk <tk@midibox.org>
2008-03-26 22:09:49 +0000

    setFillsViewportHeight temporary disabled (not available at MacOS)

--- java/org/midibox/sidlibr/gui/BankTable.java
+++ java/org/midibox/sidlibr/gui/BankTable.java
@@ -68,7 +68,9 @@ public class BankTable extends JPanel implements TableModelListener, ListSelecti
 		table.getColumnModel().getColumn(2).setPreferredWidth(70);
 		
 		table.setPreferredScrollableViewportSize(new Dimension(table.getPreferredSize().width, Math.round(table.getPreferredSize().height/4)));
-	    table.setFillsViewportHeight(true);
+
+		// TK: temporary commented out - not available under MacOS, we need a replacement
+		// table.setFillsViewportHeight(true);
 	    
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setPreferredSize(new Dimension(table.getPreferredSize().width, Math.round(table.getPreferredSize().height/4)));
