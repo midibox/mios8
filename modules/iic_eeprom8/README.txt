a4c8b1119387eec93337883e5de173b88e3be7d7
tk <tk@midibox.org>
2008-07-29 22:06:15 +0000

    programming routines for IIC eeprom w/ 8bit addresses

---
+++ modules/iic_eeprom8/README.txt
@@ -0,0 +1,54 @@
+$Id$
+
+Driver to access an external IIC EEPROM with 8bit addressing range
+
+For accessing EEPROMs with 16bit address range, use the common
+MIOS_BANKSTICK_* routines
+
+
+Files
+~~~~~
+
+  iic_eeprom8.inc: can be included into "asm-only" programs (non-relocatable code)
+  iic_eeprom8.asm: relocatable wrapper for C based applications
+  iic_eeprom8.h:   C headers
+  iic_eeprom8.mk:  include this into Makefile to integrate the module
+
+
+Functions
+~~~~~~~~~
+
+  see function heads in iic_eeprom8.inc, and C declarations in iic_eeprom.h
+
+
+Defines
+~~~~~~~
+
+  none
+
+
+Integration Hints
+~~~~~~~~~~~~~~~~~
+
+C based Applications
+~~~~~~~~~~~~~~~~~~~~
+
+  1) Makefile: add "iic_eeprom8.o" to OBJS variable
+
+  2) Makefile: include iic_eeprom8.mk
+---
+# include IIC_EEPROM8 driver
+include $(MIOS_PATH)/modules/iic_eeprom8/iic_eeprom8.mk
+---
+
+  3) main.c: #include <iic_eeprom8.h>
+
+
+
+"Assembler-Only" Applications
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  1) main.asm (or main.inc): #include <iic_eeprom8.inc>
+
+  2) Makefile: include iic_eeprom8.mk
+
---
+++ modules/iic_eeprom8/iic_eeprom8.asm
@@ -0,0 +1,45 @@
+; $Id$
+;
+;  Wrapper for iic_eeprom8.inc
+;  To be used for relocatable programs
+;
+
+; ==========================================================================
+
+	radix	dec
+#include <mios.h>
+#include <mios_vectors.inc>
+#include <macros.h>
+
+; ==========================================================================
+
+	;; export lables
+	global	IIC_EEPROM8_Write
+	global	IIC_EEPROM8_Read
+
+	global	_IIC_EEPROM8_Write
+	global	_IIC_EEPROM8_Read
+
+; ==========================================================================
+;  Declare variables 
+; ==========================================================================
+
+; ==========================================================================
+IIC_EEPROM8 code
+#include <iic_eeprom8.inc>
+
+; ==========================================================================
+	;; C function wrappers
+
+_IIC_EEPROM8_Write
+	movwf	MIOS_PARAMETER1
+        movff   FSR0L, FSR2L
+        movf    PREINC2, W
+	rgoto	IIC_EEPROM8_Write
+	
+_IIC_EEPROM8_Read
+	movwf	MIOS_PARAMETER1
+	rgoto	IIC_EEPROM8_Read
+
+; ==========================================================================
+	END
