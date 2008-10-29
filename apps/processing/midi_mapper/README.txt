3a9460f46ca3b02495668bad63f3f7a24fa5d07a
this <thismaechler@gmail.com>
2008-10-29 11:44:46 +0000

    First working version

--- apps/processing/midi_mapper/README.txt
+++ apps/processing/midi_mapper/README.txt
@@ -1,7 +1,7 @@
+MIDIbox MIDI-Mapper
 
-<application name>
 ===============================================================================
-Copyright (C) <year>  <name> (<email>)
+Copyright (C) 2008  Matthias Mächler (maechler@mm-computing.ch / thismaechler@gmx.ch)
 Licensed for personal non-commercial use only.
 All other rights reserved.
 ===============================================================================
@@ -19,13 +19,42 @@ The details are described under
 ===============================================================================
 
 Required hardware:
-   o one MBHP_CORE module
+   - one MBHP_CORE module
+	- one MBHP_DIN module
+	- one MBHP_DOUT module
+	- 20 buttons(NOT switches!) with LED's, or separate LED's
+	- 1 32K or 64K Bankstick
+	- AC or DC power supply, 7-10V, 800mA. If the device reboots when much
+	  LED's are switched on, then you need a more powerfull supply.
+	- Switch to turn the device on / off.
 
 Optional hardware:
    o 
 
 ===============================================================================
 
-<description>
+The device maps MIDI-input-chanels to MIDI-output-channels.
+Each of the 16 input / output channels can be assigned to
+any of the 16 buses. So serveral output channels can be fed
+by one input channel, or serveral input channels can go to
+one or more output channels. Redundancy over the buses will
+be solved, a input->output map will be built to forward the
+messages.
+A setup can be stored in one of the 16 presets. There are 
+four "screen"-buttons and 16 "value"-buttons:
+
+- preset screen: DIN PIN 0, DOUT PIN 0(LED)
+- bus screen: DIN PIN 1, DOUT PIN 1
+- input screen: DIN PIN 2, DOUT PIN 2
+- output screen: DIN PIN 3, DOUT PIN 3
+
+- value-buttons (1-16), DIN 8-23, DOUT 8-23(LEDS)
+
+all functionality can be configured by these 20 buttons,
+the LED's indicate the current state/setup, therefore no
+LCD-display is needed.
+
+
+
 
 ===============================================================================
--- apps/processing/midi_mapper/main.c
+++ apps/processing/midi_mapper/main.c
@@ -32,11 +32,9 @@ struct midi_bus{
 	
 //--constants--
 
-//--1 tick = 52.428ms; 20 ticks = ca. 1 sec--
+//--1 tick = 10ms; 100 ticks == 1 sec--
 //time that the user has to hold the preset/bus button down to init all presets/all buses
-#define init_request_ticks 60
-//time that all the 16 value-leds will flash after a init
-#define init_ledflash_ticks 7
+#define init_request_ticks 300
 
 
 const unsigned int int_bit_ormask[16] = {
@@ -52,41 +50,104 @@ const unsigned int int_bit_ormask[16] = {
 unsigned char current_screen_num=0;//0:prs;1:bus;2:in;3:out;stored at 0x00 in EEPROM
 unsigned char current_preset_num=0;//stored at 0x01 of EEPROM
 unsigned char current_bus_num=0;//stored at 0x02 of EEPROM
+unsigned char current_shift_state=0;
+
+//--timed functions: 1: request init; 2:LED flash
+unsigned char timed_function=0;
+unsigned int timer_ticks_count=0;
+
+//--LED flash--
+unsigned char led_flash_led=0;
+unsigned char led_flash_count=0;
+unsigned char led_flash_ticks=0;
 
 struct midi_bus current_preset[16];//stored in Bank Stick after change
 signed char midi_inout_map[16][16];//map the input-> output relations for faster msg forwarding
 
 
-//--timer--
-unsigned char timer_ticks_count=0;
-
 //------------------------------------------
 //----------application functions-----------
 //------------------------------------------
 
-void store_preset(unsigned char num) __wparam{
-	MIOS_BANKSTICK_WritePage(num*64, current_preset);
+void preset_load(unsigned char) __wparam;
+void preset_store(unsigned char) __wparam;
+void current_screen_set(unsigned char) __wparam;
+void current_bus_set(unsigned char) __wparam;
+void current_preset_set(unsigned char) __wparam;
+void inout_map_build(void) __wparam;
+void screen_print(void) __wparam;
+void timed_function_start(unsigned char) __wparam;
+void timed_function_stop(void) __wparam;
+void led_flash_start(unsigned char,unsigned char,unsigned char) __wparam;
+void led_flash_set(unsigned char) __wparam;
+
+
+void Init(void) __wparam{
+  	MIOS_SRIO_UpdateFrqSet(3); // ms
+  	MIOS_SRIO_NumberSet(3);//need 20 inputs / outputs 
+  	MIOS_SRIO_DebounceSet(20);
+  	MIOS_BANKSTICK_CtrlSet(0x40);//stick 0, verify disabled
+  	//load last application state
+  	current_screen_num = MIOS_EEPROM_Read(0x00);
+  	current_preset_num = MIOS_EEPROM_Read(0x01);
+  	current_bus_num = MIOS_EEPROM_Read(0x02);
+  	preset_load(current_preset_num);
+  	//init screen
+  	screen_print();
+  	}
+
+
+//---------------preset load/store/build map/init------------
+
+void preset_load(unsigned char num) __wparam{
+	unsigned int addr=num;
+	addr <<=6;
+	MIOS_BANKSTICK_ReadPage(addr,current_preset);
+	}
+
+
+void preset_store(unsigned char num) __wparam{
+	unsigned int addr=num;
+	addr <<=6;
+	MIOS_BANKSTICK_WritePage(addr,current_preset);	
 	}
 	
-void load_preset(unsigned char num) __wparam{
-	unsigned char bus,chn_in,chn_out,i;	
-	MIOS_BANKSTICK_ReadPage(num*64, current_preset);	
-	//extraxt midi_inout_map
-	for(bus=0;bus<16;bus++){
-		for(chn_in=0;chn_in<16;chn_in++){
-			if(current_preset[bus].inputs & int_bit_ormask[chn_in]){				
-				i=0;				
-				for(chn_out=0;chn_out<16;chn_out++){
-					if(current_preset[bus].outputs & int_bit_ormask[chn_out])
-						midi_inout_map[chn_in][i++] = chn_out;
-					}
-				midi_inout_map[chn_in][i] = -1;//terminate list
+void preset_init(unsigned char empty) __wparam{
+	unsigned char i;
+	for(i=0;i<16;i++){
+		current_preset[i].inputs = current_preset[i].outputs = (empty ? 0 : int_bit_ormask[i]);
+		midi_inout_map[i][0]= (empty ? 0xff : i);
+		midi_inout_map[i][1]=0xff;//terminate		
+		}
+	}
+	
+void inout_map_build(void) __wparam{
+	unsigned char input,output,bus,i;
+	unsigned int outputs[16];
+	for(input=0;input<16;input++)
+		outputs[input] = 0;
+	for(input=0;input<16;input++){
+		for(bus=0;bus<16;bus++){
+			for(input=0;input<16;input++){
+				if(current_preset[bus].inputs & int_bit_ormask[input])
+					outputs[input] |= current_preset[bus].outputs;
 				}
 			}
 		}
+	for(input=0;input<16;input++){
+		i=0;
+		for(output=0;output<16;output++)
+			if(outputs[input]&int_bit_ormask[output])
+				midi_inout_map[input][i++]=output;
+		midi_inout_map[input][i] = 0xff;
+		}
 	}
+	
+//-------------screen-------------------
 		
-void screen_init(void) __wparam{
+void screen_print(void) __wparam{
+	if(timed_function==2)
+		return;//no screen print on LED flash
 	//set bitmask for screen button
 	MIOS_DOUT_SRSet(0x00,MIOS_HLP_GetBitORMask(current_screen_num));
 	//clear value screen
@@ -95,9 +156,9 @@ void screen_init(void) __wparam{
 	switch(current_screen_num){
 		case 0://preset screen
 			if(current_preset_num < 8)			
-				MIOS_DOUT_SRSet(0x01,MIOS_HLP_GetBitORMask(current_screen_num));
+				MIOS_DOUT_SRSet(0x01,MIOS_HLP_GetBitORMask(current_preset_num));
 			else
-				MIOS_DOUT_SRSet(0x02,MIOS_HLP_GetBitORMask(current_screen_num-8));				
+				MIOS_DOUT_SRSet(0x02,MIOS_HLP_GetBitORMask(current_preset_num-8));				
 			break;
 		case 1://bus screen
 			if(current_bus_num < 8)			
@@ -107,75 +168,178 @@ void screen_init(void) __wparam{
 			break;
 		case 2://inputs screen
 			MIOS_DOUT_SRSet(0x01,(unsigned char)(current_preset[current_bus_num].inputs & 0x00ff));
-			MIOS_DOUT_SRSet(0x02,(unsigned char)(current_preset[current_bus_num].inputs >>8 & 0x00ff));
+			MIOS_DOUT_SRSet(0x02,(unsigned char)((current_preset[current_bus_num].inputs >>8) & 0x00ff));
 			break;
 		case 3://outputs screen
 			MIOS_DOUT_SRSet(0x01,(unsigned char)(current_preset[current_bus_num].outputs & 0x00ff));
-			MIOS_DOUT_SRSet(0x02,(unsigned char)(current_preset[current_bus_num].outputs >>8 & 0x00ff));
+			MIOS_DOUT_SRSet(0x02,(unsigned char)((current_preset[current_bus_num].outputs >>8) & 0x00ff));
 		}
 	}
-
-//inits and stores the current preset. bus0:in_ch,out_ch0;bus1:in_ch1,out_ch1;..
-void preset_init(void) __wparam{
-	unsigned char i;
-	for(i=0;i<16;i++){
-		current_preset[i].inputs = current_preset[i].outputs = int_bit_ormask[i];
-		midi_inout_map[i][0]=i;
-		midi_inout_map[i][0]=-1;//terminate		
-		}
-	store_preset(current_preset_num);	
+	
+void current_screen_set(unsigned char value) __wparam{
+	current_screen_num = value;
+	MIOS_EEPROM_Write(0x00, current_screen_num);		
+	}
+	
+void current_bus_set(unsigned char value) __wparam{
+	current_bus_num = value;
+	MIOS_EEPROM_Write(0x02,value);
+	}
+	
+void current_preset_set(unsigned char value) __wparam{
+	current_preset_num = value;
+	preset_load(value);
+	inout_map_build();
+	MIOS_EEPROM_Write(0x01,value);
 	}
 
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS after startup to initialize the 
-// application
-/////////////////////////////////////////////////////////////////////////////
-void Init(void) __wparam{
-  	MIOS_SRIO_UpdateFrqSet(1); // ms
-  	MIOS_SRIO_NumberSet(3);//need 20 inputs / outputs 
-  	MIOS_SRIO_DebounceSet(10);
-  	MIOS_BANKSTICK_CtrlSet(0x00);//stick 0, verify enabled
-  	//load last application state
-  	current_screen_num = MIOS_EEPROM_Read(0x00);
-  	current_preset_num = MIOS_EEPROM_Read(0x01);
-  	current_bus_num = MIOS_EEPROM_Read(0x02);
-  	load_preset(current_preset_num);
-  	//init screen
-  	screen_init();
-  	}
+	
+//----------timed functions----------------
 
-/////////////////////////////////////////////////////////////////////////////
-// This function is periodically called by MIOS. The frequency has to be
-// initialized with MIOS_Timer_Set
-/////////////////////////////////////////////////////////////////////////////
 void Timer(void) __wparam{
+	timer_ticks_count++;
+	}
+
+
+void timed_function_start(unsigned char func) __wparam{
+	timer_ticks_count=0;
+	timed_function = func;
+	MIOS_TIMER_Init(0x01,50000);
+	}
+	
+void timed_function_stop(void) __wparam{
+	MIOS_TIMER_Stop();
+	timed_function = 0;
+	timer_ticks_count = 0;
+	}
+	
+void led_flash_start(unsigned char led,unsigned char times,unsigned char ticks) __wparam{
+	led_flash_led = led;
+	led_flash_ticks = ticks;
+	led_flash_count = (times<<1) + 1;
+	led_flash_set(0);
+	timed_function_start(2);
+	}
+	
+void led_flash_set(unsigned char state) __wparam{
+	if(led_flash_led == 0xff){
+		state = (state ? 0xff : 0x00);
+		MIOS_DOUT_SRSet(0x01,state);
+		MIOS_DOUT_SRSet(0x02,state);
+		}
+	else
+		MIOS_DOUT_PinSet(led_flash_led,state);
+	}
+
+
+void Tick(void) __wparam{
 	unsigned char i;
-	if(++timer_ticks_count >= init_request_ticks){
-		if(timer_ticks_count == init_request_ticks){
-			preset_init();//inits the current preset
-			if(!current_screen_num){//page is preset.. 
-				for(i=0;i<16;i++)//inits all presets on bankstick(store reset preset)
-					store_preset(i);
-				}	
-			else//page is bank so..
-				store_preset(current_preset_num);//store the current (reset) preset
-			//flash!
-			MIOS_DOUT_SRSet(0x01,0xff);
-			MIOS_DOUT_SRSet(0x02,0xff);
+	if(!timed_function)
+		return;
+	if(timed_function==1 && (timer_ticks_count >= init_request_ticks)){
+		timed_function_stop();
+		if(!current_screen_num){//screen is preset
+			preset_init(0);
+			preset_store(0);
+			preset_init(1);
+			for(i=1;i<16;i++)
+				preset_store(i);
+			current_preset_set(0);
+			current_bus_set(0);
+			inout_map_build();
+			led_flash_start(0xff,5,10);//flash 5 times
 			}
-		else if(timer_ticks_count >= init_request_ticks+init_ledflash_ticks){
-			MIOS_TIMER_Stop();//stops the init request countdown
-			screen_init();
+		else{//screen is bank
+			preset_init(1);//empty current preset
+			current_bus_set(0);
+			led_flash_start(0xff,2,10);//flash 2 times
+			}
+		}
+	else if(timed_function==2 && (timer_ticks_count >= led_flash_ticks)){
+		if(led_flash_count > 1){
+			led_flash_set(led_flash_count-- & 0x01);
+			timer_ticks_count = 0;
+			}
+		else{
+			timed_function_stop();
+			screen_print();
 			}
 		}
 	}
 
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS when a MIDI byte has been received
-/////////////////////////////////////////////////////////////////////////////
+	
+//------------------button handling-----------------------
 
+void DIN_NotifyToggle(unsigned char pin, unsigned char pin_value) __wparam{
+	unsigned char i,chn_out,chn_in,value;
+	if(timed_function==1)
+		timed_function_stop();//stop clear request on every button change
+	else if(timed_function==2 && !pin_value)
+		return;//button push will only be processed when no LED's are flashing.
+	if(pin > 7 && !pin_value){//this is a value button. only handle if button is pushed
+		value = pin - 8;
+		switch(current_screen_num){
+			case 0://preset screen
+				if (current_shift_state & int_bit_ormask[0]){
+					preset_store(current_preset_num);
+					led_flash_start(current_preset_num+8,0,50);//LED off for half a second to indicate save
+					}
+				else
+					current_preset_set(value);
+				break;			
+			case 1://bus screen
+				current_bus_set(value);
+				break;			
+			case 2://input screen
+				if (current_shift_state & int_bit_ormask[2]){//add/remove input chanel
+					current_preset[current_bus_num].inputs = 
+						(current_preset[current_bus_num].inputs & int_bit_ormask[value])?
+							(current_preset[current_bus_num].inputs ^ int_bit_ormask[value]):
+							(current_preset[current_bus_num].inputs | int_bit_ormask[value]);
+					}
+				else{//set / unset input channel
+					current_preset[current_bus_num].inputs = 
+						(current_preset[current_bus_num].inputs==int_bit_ormask[value]) ? 0x0000 :int_bit_ormask[value];
+					}		
+				inout_map_build();
+				break;			
+			case 3://output screen
+				if (current_shift_state & int_bit_ormask[3]){//set/unset output chanel
+					current_preset[current_bus_num].outputs = 
+						(current_preset[current_bus_num].outputs & int_bit_ormask[value])?
+							(current_preset[current_bus_num].outputs ^ int_bit_ormask[value]):
+							(current_preset[current_bus_num].outputs | int_bit_ormask[value]);
+					}
+				else{//add/remove output chanel
+					current_preset[current_bus_num].outputs = 
+						(current_preset[current_bus_num].outputs==int_bit_ormask[value]) ? 
+							0x0000 :int_bit_ormask[value];
+					}				
+				inout_map_build();
+				break;
+			}
+		}
+	else if(pin < 4){
+		if(pin_value)
+			current_shift_state &= ~int_bit_ormask[pin];//remove shift flag
+		else{
+			if(!current_shift_state){
+				current_screen_set(pin);
+				if(pin < 2)//start init request countdown
+					timed_function_start(1);
+				}
+			current_shift_state |= int_bit_ormask[pin];//add shift flag
+			}
+		}
+	screen_print();
+	}
+	
+
+
+//----------------MIDI Bytes / Events receive-------------------------------
+
+//this function forwards all system messages to the output
 void MPROC_NotifyReceivedByte(unsigned char byte) __wparam{
-	//this function forwards all system messages to the output
 	static unsigned char fx_status = 0;
 	if(byte >= 0xf0){//system status byte
 		MIOS_MIDI_TxBufferPut(byte);
@@ -202,9 +366,7 @@ void MPROC_NotifyReceivedByte(unsigned char byte) __wparam{
    	}
  	}
 
-/////////////////////////////////////////////////////////////////////////////
-//  This function is called by MIOS when a complete MIDI event has been received
-/////////////////////////////////////////////////////////////////////////////
+
 void MPROC_NotifyReceivedEvnt(unsigned char evnt0, unsigned char evnt1, unsigned char evnt2) __wparam{
 	unsigned char i;
 	unsigned char evnt_type = evnt0 & 0xf0;
@@ -218,142 +380,31 @@ void MPROC_NotifyReceivedEvnt(unsigned char evnt0, unsigned char evnt1, unsigned
 		}
 	}
 
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS when an button has been toggled
-// pin_value is 1 when button released, and 0 when button pressed
-/////////////////////////////////////////////////////////////////////////////
-void DIN_NotifyToggle(unsigned char pin, unsigned char pin_value) __wparam{
-	unsigned char i,chn_out,chn_in,value;
-	MIOS_TIMER_Stop();//stops the init request countdown
-	if(pin_value) return;//button up, nothing more to do	
-	if(pin > 7){
-		value = pin - 8;
-		switch(current_screen_num){
-			case 0://preset screen
-				current_preset_num = value;				
-				load_preset(current_preset_num);
-				MIOS_EEPROM_Write(0x01,current_preset_num);
-				break;			
-			case 1://bus screen
-				current_bus_num = value;
-				MIOS_EEPROM_Write(0x02,current_bus_num);
-				break;			
-			case 2://input screen
-				if (MIOS_DIN_PinGet(0x02)){//set/unset input chanel
-					current_preset[current_bus_num].inputs = 
-						current_preset[current_bus_num].inputs==int_bit_ormask[value] ? 0x0000 :int_bit_ormask[value];
-					}
-				else{//add/remove input chanel
-					current_preset[current_bus_num].inputs = 
-						(current_preset[current_bus_num].inputs & int_bit_ormask[value])?
-							(current_preset[current_bus_num].inputs ^ int_bit_ormask[value]):
-							(current_preset[current_bus_num].inputs | int_bit_ormask[value]);
-					}		
-				//assign the bus's output channels to new input channel on the map			
-				i=0;	
-				for(chn_out=0;chn_out<16;chn_out++){
-					if(current_preset[current_bus_num].outputs & int_bit_ormask[chn_out])//this is a bus output
-						midi_inout_map[value][i++] = chn_out;
-					}
-				midi_inout_map[value][i] = -1;
-				break;			
-			case 3://output screen
-				if (MIOS_DIN_PinGet(0x03)){//set/unset output chanel
-					current_preset[current_bus_num].outputs = 
-						(current_preset[current_bus_num].outputs==int_bit_ormask[value]) ? 
-							0x0000 :int_bit_ormask[value];
-					}
-				else{//add/remove output chanel
-					current_preset[current_bus_num].outputs = 
-						(current_preset[current_bus_num].outputs & int_bit_ormask[value])?
-							(current_preset[current_bus_num].outputs ^ int_bit_ormask[value]):
-							(current_preset[current_bus_num].outputs | int_bit_ormask[value]);
-					}				
-				//assign the outputchannels to the bus's input-channels on the map  			
-				for(chn_in=0;chn_in<16;chn_in++){
-					if(current_preset[current_bus_num].inputs & int_bit_ormask[chn_in]){//this is a bus input
-						i=0;						
-						for(chn_out=0;chn_out<16;chn_out++){
-							if(current_preset[current_bus_num].outputs & int_bit_ormask[chn_out])//this is a bus output
-								midi_inout_map[chn_in][i++] = chn_out;
-							}
-						midi_inout_map[chn_in][i] = -1;
-						}						
-					}
-				break;
-			}
-		}
-	else if(pin < 4){
-		current_screen_num = pin;
-		MIOS_EEPROM_Write(0x00, current_screen_num);
-		if(pin < 2){//start init request countdown
-			timer_ticks_count = 0;			
-			MIOS_TIMER_Init(0x03,0xffff);
-			}			 			
-		}
-	screen_init();
-	}
-	
 
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS when the display content should be 
-// initialized. Thats the case during startup and after a temporary message
-// has been printed on the screen
-/////////////////////////////////////////////////////////////////////////////
+
+
+//---------------------------not used in this application----------------
+
 void DISPLAY_Init(void) __wparam{
 	}
 
-/////////////////////////////////////////////////////////////////////////////
-//  This function is called in the mainloop when no temporary message is shown
-//  on screen. Print the realtime messages here
-/////////////////////////////////////////////////////////////////////////////
 void DISPLAY_Tick(void) __wparam{
 	}
-	
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS in the mainloop when nothing else is to do
-/////////////////////////////////////////////////////////////////////////////
-void Tick(void) __wparam{
-	}
-	
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS when a MIDI event has been received
-// which has been specified in the MIOS_MPROC_EVENT_TABLE
-/////////////////////////////////////////////////////////////////////////////
+
 void MPROC_NotifyFoundEvent(unsigned entry, unsigned char evnt0, unsigned char evnt1, unsigned char evnt2) __wparam{
 	}
 
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS when a MIDI event has not been completly
-// received within 2 seconds
-/////////////////////////////////////////////////////////////////////////////
 void MPROC_NotifyTimeout(void) __wparam{
 	}
 
-	
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS before the shift register are loaded
-/////////////////////////////////////////////////////////////////////////////
 void SR_Service_Prepare(void) __wparam{
 	}
 
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS after the shift register have been loaded
-/////////////////////////////////////////////////////////////////////////////
 void SR_Service_Finish(void) __wparam{
 	}
 
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS when an encoder has been moved
-// incrementer is positive when encoder has been turned clockwise, else
-// it is negative
-/////////////////////////////////////////////////////////////////////////////
 void ENC_NotifyChange(unsigned char encoder, char incrementer) __wparam{
 	}
 
-/////////////////////////////////////////////////////////////////////////////
-// This function is called by MIOS when a pot has been moved
-/////////////////////////////////////////////////////////////////////////////
 void AIN_NotifyChange(unsigned char pin, unsigned int pin_value) __wparam{
 	}
-	
