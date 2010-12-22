6764bc8388e3aa149d5d88e69cc453f0a35dd498
tk <tk@midibox.org>
2010-12-22 14:31:43 +0000

    repaired asm_skeleton

---
+++ apps/templates/asm_skeleton/app_defines.h
@@ -0,0 +1,14 @@
+;$Id$
+;
+; Specific Defines and variables of MIOS Application
+;
+; ==========================================================================
+;
+;  Copyright (C) <year>  <name> (<email>)
+;  Licensed for personal non-commercial use only.
+;  All other rights reserved.
+; 
+; ==========================================================================
+
+	;; free memory for user applications:	
+	;; 0x010-0x37f
